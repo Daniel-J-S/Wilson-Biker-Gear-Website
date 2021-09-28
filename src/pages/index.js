c7773c7c4ac3d583467f8fdf94bc601e835a28d2
myDeveloperJourney <daniel@mydeveloperjourney.com>
2021-09-28 14:04:37 -0500

    wip: adds coming soon page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,159 +9,177 @@ import Countdown from "../components/countdown"
 import StarRatingComponent from 'react-star-rating-component';
 import { graphql } from "gatsby";
 
-class IndexPost extends React.Component {
-  render() {
-    const { data } = this.props;
-    return (
-      <React.Fragment>
-        <div className="row product-main">
-          {data.data.allContentfulProduct.edges.map(items => (
-            <Link className="Catalogue__item col-sm-12 col-md-6 col-lg-4" to={`/${items.node.slug}`}>
-            <div key={items.node.id}>
-              <div className="details_List">
-                {items.node.image === null ? <div className="no-image">No Image</div> : <Img sizes={items.node.image.fluid} />}
 
-                <div className="details_inner">
-
-                  <h2>
-                    {items.node.name}
-                  </h2>
-                  <StarRatingComponent
-                    name="rate1"
-                    starCount={5}
-                    value={items.node.rating}
-                  />
-                  <p>{items.node.description.childMarkdownRemark.excerpt}</p>
-                  <div className="row">
-                    <div className="col-sm-4 align-self-center">
-                      <span className="price">${items.node.price}</span>
-                    </div>
-                    <div className="col-sm-8 text-right align-self-center">
-                      {/* <a
-                        href="#"
-                        className="Product snipcart-add-item"
-                        data-item-id={items.node.slug}
-                        data-item-price={items.node.price}
-                        data-item-image={items.node.image === null ? "" : items.node.image.fluid.src}
-                        data-item-name={items.node.name}
-                        data-item-url={`/`}
-                      >
-                        <i className="fas fa-shopping-bag" />Add to Cart
-                    </a> */}
-                    <Link state={{ 
-                      itemName: items.node.name,
-                      itemPrice: items.node.price
-                      }} className="btn btn-primary" to="/contact-us">Contact Us</Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            </Link>
-          ))}
-        </div>
-      </React.Fragment>
-    );
-  }
+function ComingSoon() {
+  return (
+      <div style={{
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'center', 
+          alignItems: 'center',
+          minHeight: '100vh'
+          }}>
+        <h1>COMING SOON</h1>
+        <p>This website is under construction.</p>
+      </div>
+  )
 }
 
-const IndexPage = data => (
-  <Layout>
-    <SEO title="Home" keywords={[`biker gear`, `vests`, `sewing`, `jackets`]} />
-    <Banner BannerData={data.data.allContentfulHeaderBanner.edges} />
-    {/* <LatestBlogs data={data.data.allContentfulBlogs} /> */}
-    <div className="container">
-      <div className="text-center"><h2 className="with-underline">Our Latest Selections</h2></div>
-      <IndexPost data={data}></IndexPost>
-    </div>
-    <Countdown data={data.data.contentfulDealCountDown} />
-  </Layout>
-)
+export default ComingSoon
 
-export default IndexPage
+// class IndexPost extends React.Component {
+//   render() {
+//     const { data } = this.props;
+//     return (
+//       <React.Fragment>
+//         <div className="row product-main">
+//           {data.data.allContentfulProduct.edges.map(items => (
+//             <Link className="Catalogue__item col-sm-12 col-md-6 col-lg-4" to={`/${items.node.slug}`}>
+//             <div key={items.node.id}>
+//               <div className="details_List">
+//                 {items.node.image === null ? <div className="no-image">No Image</div> : <Img sizes={items.node.image.fluid} />}
 
-export const query = graphql`
-  query AboutQuery {
-    allContentfulProduct(limit: 6,sort:{fields:createdAt,order: DESC}){
-      edges{
-        node{
-          id
-          name
-          slug
-          rating
-          image {
-            fluid(maxWidth: 1000) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
-            }
-          }
-          price
-          description {
-            childMarkdownRemark {
-              excerpt(pruneLength: 140)
-            }
-          }
-        }
-      }
-    }
-    allContentfulHeaderBanner {
-      edges {
-        node {
-          title
-          subHeading
-          image {
-            fluid(maxWidth: 1800) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
-            }
-          }
-        }
-      }
-    }
-    contentfulDealCountDown {
-      title
-      featureImage {
-        fluid(maxWidth: 1800) {
-          base64
-          aspectRatio
-          src
-          srcSet
-          srcWebp
-          srcSetWebp
-          sizes
-        }
-      }
-      date(formatString: "D MMMM, YYYY")
-    }
-    allContentfulBlogs(limit: 3,sort:{fields:createdAt,order: DESC}) {
-      edges {
-        node {
-          id
-          title
-          slug
-          featureImage {
-            fluid(maxWidth: 1120) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
-            }
-          }
-        }
-      }
-    }
-  }
-`
+//                 <div className="details_inner">
+
+//                   <h2>
+//                     {items.node.name}
+//                   </h2>
+//                   <StarRatingComponent
+//                     name="rate1"
+//                     starCount={5}
+//                     value={items.node.rating}
+//                   />
+//                   <p>{items.node.description.childMarkdownRemark.excerpt}</p>
+//                   <div className="row">
+//                     <div className="col-sm-4 align-self-center">
+//                       <span className="price">${items.node.price}</span>
+//                     </div>
+//                     <div className="col-sm-8 text-right align-self-center">
+//                       {/* <a
+//                         href="#"
+//                         className="Product snipcart-add-item"
+//                         data-item-id={items.node.slug}
+//                         data-item-price={items.node.price}
+//                         data-item-image={items.node.image === null ? "" : items.node.image.fluid.src}
+//                         data-item-name={items.node.name}
+//                         data-item-url={`/`}
+//                       >
+//                         <i className="fas fa-shopping-bag" />Add to Cart
+//                     </a> */}
+//                     <Link state={{ 
+//                       itemName: items.node.name,
+//                       itemPrice: items.node.price
+//                       }} className="btn btn-primary" to="/contact-us">Contact Us</Link>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//             </Link>
+//           ))}
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// const IndexPage = data => (
+//   <Layout>
+//     <SEO title="Home" keywords={[`biker gear`, `vests`, `sewing`, `jackets`]} />
+//     <Banner BannerData={data.data.allContentfulHeaderBanner.edges} />
+//     {/* <LatestBlogs data={data.data.allContentfulBlogs} /> */}
+//     <div className="container">
+//       <div className="text-center"><h2 className="with-underline">Our Latest Selections</h2></div>
+//       <IndexPost data={data}></IndexPost>
+//     </div>
+//     <Countdown data={data.data.contentfulDealCountDown} />
+//   </Layout>
+// )
+
+// export default IndexPage
+
+// export const query = graphql`
+//   query AboutQuery {
+//     allContentfulProduct(limit: 6,sort:{fields:createdAt,order: DESC}){
+//       edges{
+//         node{
+//           id
+//           name
+//           slug
+//           rating
+//           image {
+//             fluid(maxWidth: 1000) {
+//               base64
+//               aspectRatio
+//               src
+//               srcSet
+//               srcWebp
+//               srcSetWebp
+//               sizes
+//             }
+//           }
+//           price
+//           description {
+//             childMarkdownRemark {
+//               excerpt(pruneLength: 140)
+//             }
+//           }
+//         }
+//       }
+//     }
+//     allContentfulHeaderBanner {
+//       edges {
+//         node {
+//           title
+//           subHeading
+//           image {
+//             fluid(maxWidth: 1800) {
+//               base64
+//               aspectRatio
+//               src
+//               srcSet
+//               srcWebp
+//               srcSetWebp
+//               sizes
+//             }
+//           }
+//         }
+//       }
+//     }
+//     contentfulDealCountDown {
+//       title
+//       featureImage {
+//         fluid(maxWidth: 1800) {
+//           base64
+//           aspectRatio
+//           src
+//           srcSet
+//           srcWebp
+//           srcSetWebp
+//           sizes
+//         }
+//       }
+//       date(formatString: "D MMMM, YYYY")
+//     }
+//     allContentfulBlogs(limit: 3,sort:{fields:createdAt,order: DESC}) {
+//       edges {
+//         node {
+//           id
+//           title
+//           slug
+//           featureImage {
+//             fluid(maxWidth: 1120) {
+//               base64
+//               aspectRatio
+//               src
+//               srcSet
+//               srcWebp
+//               srcSetWebp
+//               sizes
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
