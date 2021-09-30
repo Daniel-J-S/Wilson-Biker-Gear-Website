ab7002b92151285f13ba1a89a8e7c294d3b058b8
Daniel JS <daniel@mydeveloperjourney.com>
2021-09-30 14:47:36 -0500

    wip: adds footer address

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,9 +20,23 @@ class Footer extends Component {
                                 </div>
                             </section>
                         </div>
-                        <div className="footer-bottom social-right-menu ">
+                        <div className="footer-bottom social-right-menu mt-5">
                             <div className="site-info">
-                                ©{new Date().getFullYear()} Wilson Biker Gear. All rights reserved.
+                                <address>
+                                    3016 Alta Mere Dr<br />
+                                    Fort Worth, TX 76116<br />
+                                    <a href="tel:8173860631">(817) 386-0631</a>
+                                </address>
+                                <p className="mt-5 mb-3"><strong>Hours of Operation</strong></p>
+                                <ul>
+                                    <li>Tues - Sat: 9 AM - 5 PM</li>
+                                    <li>Sun - Mon: CLOSED</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="footer-bottom social-right-menu mt-5">
+                            <div className="site-info">
+                               <small>©{new Date().getFullYear()} Wilson Biker Gear. All rights reserved.</small>
                             </div>
                         </div>
                     </div>
