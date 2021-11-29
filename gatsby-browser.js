5adaccbf6cd3a5fe13d609fb241200d8176219ad
myDeveloperJourney <daniel@mydeveloperjourney.com>
2021-11-29 17:05:31 -0600

    wip: adds service worker update notification

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,14 @@ import './src/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './src/css/style.css';
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `This application has been updated. ` +
+        `Reload to display the latest version?`
+    );
+  
+    if (answer === true) {
+      window.location.reload();
+    }
+};
