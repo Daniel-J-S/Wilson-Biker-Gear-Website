1adbf587063de925589c45d32339f8185bdabb70
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-03 09:36:12 -0600

    wip: adds additional updates to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ const { spaceId, accessToken, snipcart, websiteId } = process.env;
 module.exports = {
   siteMetadata: {
     title: `Wilson Biker Gear`,
-    description: `Business Website For Wilson Biker Gear`,
+    description: `We've been in the business for over 40 years selling motorcycle rider's apparel, accessories and providing sewing services`,
     author: `DanielJS`,
   },
   plugins: [
@@ -18,21 +18,21 @@ module.exports = {
         zIndex: `9999`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-layout`,
-    //   options: {
-    //     component: require.resolve(`./src/components/layout.js`),
-    //   },
-    // },
-    // {
-    //   resolve: 'gatsby-plugin-crisp-chat',
-    //   options: {
-    //     websiteId,
-    //     enableDuringDevelop: true, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
-    //     defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
-    //     enableImprovedAccessibility: true // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout.js`),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-crisp-chat',
+      options: {
+        websiteId,
+        enableDuringDevelop: true, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
+        defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
+        enableImprovedAccessibility: true // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -48,7 +48,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Wilson Bike Supply`,
-        short_name: `starter`,
+        short_name: `Wilson Bike Supply`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,
