14437b9653953c94e75e2887b48deaf2bdaa7814
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-21 10:38:15 -0600

    wip: adds latest updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,67 +1,96 @@
-import React, { Component } from 'react';
-import { Link } from 'gatsby';
-
+import React from 'react';
+import { Link, navigate } from 'gatsby';
+import { isBrowser } from '../utils/isBrowser';
 import tiktokLogo from '../static/tiktok-brands.svg';
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer className="site-footer">
-                <div className="footer_inner">
-                    <div className="container">
-                        <div className="footer-widget footer-content">
-                            <section id="nav_menu-8" className="widget widget_nav_menu">
-                                <div className="menu-main-container">
-                                    <ul id="menu-main" className="menu">
-                                        <li><Link to="/about">About</Link></li>
-                                        <li><Link to="/store">Store</Link></li>
-                                        <li><Link to="/terms-of-service">Terms of Service</Link></li>
-                                        <li><Link to="/contact-us">Contact</Link></li>
-                                    </ul>
-                                </div>
-                            </section>
-                        </div>
-                        <div className="footer-bottom social-right-menu mt-3">
-                            <div className="site-info">
-                                <ul className="social-links">
-                                    <li>
-                                        <a className="social-icon" href="https://www.facebook.com/JDUBORIGINAL/" target="_blank" rel="noopener noreferrer">
-                                            <i className="fab fa-2x fa-facebook" />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="social-icon" href="https://www.tiktok.com/@wilsonbikergear" target="_blank" rel="noopener noreferrer">                                            
-                                            <img className="tiktok" src={tiktokLogo} alt="tic toc" />
-                                        </a>
-                                    </li>
+const PublishButton = ({ handleClick }) => {
+    return (
+        <button onClick={handleClick} style={{
+            backgroundColor: '#000',
+            color: '#fff',
+            border: 'none',
+            margin: '2rem 0',
+            fontFamily: 'Helvetica',
+            borderBottom: '2px solid dodgerblue',
+        }}>
+            Publish This Version
+        </button>
+    )
+}
+
+function Footer () {
+    const handleClick = async () => {
+        if (!isBrowser) return;
+        const proceed = window.confirm(`🚨 Please confirm that you are ready to publish the current changes`);
+        if(proceed) {
+            const url = 'https://api.netlify.com/build_hooks/61563cb76500392b3573d5ba';
+            await fetch(url, { method: 'POST'});
+            window.alert(`
+            🚧 Changes are currently being migrated to production.
+            Please allow 3-5 mins for changes to reflect 🚧
+            `);
+            navigate('/');
+        }
+    }
+    return (
+        <footer className="site-footer">
+            <div className="footer_inner">
+                <div className="container">
+                    <div className="footer-widget footer-content">
+                        <section id="nav_menu-8" className="widget widget_nav_menu">
+                            <div className="menu-main-container">
+                                <ul id="menu-main" className="menu">
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/store">Store</Link></li>
+                                    <li><Link to="/terms-of-service">Terms of Service</Link></li>
+                                    <li><Link to="/contact-us">Contact</Link></li>
                                 </ul>
-                                <address className="mt-3">
-                                    <a href="https://g.page/wilson-biker-gear?share" target="_blank" rel="noopener noreferrer">
-                                    3016 Alta Mere Dr<br />
-                                    Fort Worth, TX 76116<br />
+                            </div>
+                        </section>
+                    </div>
+                    <div className="footer-bottom social-right-menu mt-3">
+                        <div className="site-info">
+                            <ul className="social-links">
+                                <li>
+                                    <a className="social-icon" href="https://www.facebook.com/JDUBORIGINAL/" target="_blank" rel="noopener noreferrer">
+                                        <i className="fab fa-2x fa-facebook" />
                                     </a>
-                                    <a href="tel:8173860631">(817) 386-0631</a>
-                                </address>
-                                <p className="mt-5 mb-3"><strong>Hours of Operation</strong></p>
-                                <ul>
-                                    <li>Weds - Sun: 9 AM - 5 PM</li>
-                                    <li>Mon - Tues: CLOSED</li>
-                                </ul>
-                            </div>
+                                </li>
+                                <li>
+                                    <a className="social-icon" href="https://www.tiktok.com/@wilsonbikergear" target="_blank" rel="noopener noreferrer">                                            
+                                        <img className="tiktok" src={tiktokLogo} alt="tic toc" />
+                                    </a>
+                                </li>
+                            </ul>
+                            <address className="mt-3">
+                                <a href="https://g.page/wilson-biker-gear?share" target="_blank" rel="noopener noreferrer">
+                                3016 Alta Mere Dr<br />
+                                Fort Worth, TX 76116<br />
+                                </a>
+                                <a href="tel:8173860631">(817) 386-0631</a>
+                            </address>
+                            <p className="mt-5 mb-3"><strong>Hours of Operation</strong></p>
+                            <ul>
+                                <li>Weds - Sun: 9 AM - 5 PM</li>
+                                <li>Mon - Tues: CLOSED</li>
+                            </ul>
                         </div>
-                        <div className="footer-bottom social-right-menu mt-5">
-                            <div className="site-info">
-                               <small>©{new Date().getFullYear()} Wilson Biker Gear. All rights reserved.</small>
-                            </div>
-                            <div className="site-info">
-                               <small>Custom Built By <a href="https://danieljs.io" target="_blank" rel="noopener noreferrer">DanielJS</a></small>
-                            </div>
+                    </div>
+                    <div className="footer-bottom social-right-menu mt-5">
+                        <div className="site-info">
+                            <small>©{new Date().getFullYear()} Wilson Biker Gear. All rights reserved.</small>
+                        </div>
+                        <div className="site-info">
+                            <small>Custom Built By <a href="https://danieljs.io" target="_blank" rel="noopener noreferrer">DanielJS</a></small>
+                        </div>
+                        <div className="site-info">
+                            <PublishButton handleClick={handleClick} />
                         </div>
                     </div>
                 </div>
-            </footer>
-        )
-    }
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-
+import { isBrowser } from '../utils/isBrowser';
 import Header from './header';
 import Footer from './footer';
 
 
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children }) => { 
+  if(isBrowser) {
+    window.alert(`
+    🚨 Please Be Advised:
+    This is a test site and is only used
+    for staging future changes before adding
+    them to production.🚧
+    `)
+  }
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div className="main-container">
-          <main>{children}</main>
-        </div>
-        <Footer />
-      </>
-    )}
-  />
-);
+      `}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div className="main-container">
+            <main>{children}</main>
+          </div>
+          <Footer />
+        </>
+      )}
+    />
+  );
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
