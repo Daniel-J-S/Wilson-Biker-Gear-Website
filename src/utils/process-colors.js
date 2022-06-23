668e12bf809dd3cefb8a64d0de46e4413037f9d1
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-23 15:50:56 -0500

    wip: cleans up code and add missing semicolons

--- src/utils/process-colors.js
+++ src/utils/process-colors.js
@@ -1,9 +1,8 @@
 export function processColors(colors, color) {
     if(!colors) return colors;
     if(color) {
-        colors = colors.filter(c => c !== color)
+        colors = colors.filter(c => c !== color);
         colors.unshift(color);
     }
-    console.log(colors)
-    return colors.join("|")
+    return colors.join("|");
 }
